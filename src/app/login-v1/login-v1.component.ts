d7afc4c30966b5e5dc6e698b2031a3549e614c49
Lakshakumara <lakshakumara@gmail.com>
2025-04-09 06:13:15 +0530

    2024-04-10

--- src/app/login-v1/login-v1.component.ts
+++ src/app/login-v1/login-v1.component.ts
@@ -7,8 +7,6 @@ import { LoaderService } from '../service/loader.service';
 import { Utils } from '../util/utils';
 import Swal from 'sweetalert2';
 import { Scheme } from '../Model/scheme';
-import { Observable, of, delay, switchMap } from 'rxjs';
-import { Constants } from '../util/constants';
 
 @Component({
   selector: 'app-login-v1',
